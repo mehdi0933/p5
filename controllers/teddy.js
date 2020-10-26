16da8ef2022b6b265902f6607f918281037c6296
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2020-10-26 15:28:45 +0100

    Add files via upload

--- controllers/teddy.js
+++ controllers/teddy.js
@@ -90,3 +90,33 @@ exports.orderTeddies = (req, res, next) => {
     }
   );
 };
+function showCustomer(str) {
+  var xhttp;
+  if (str == "") {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  }
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("txtHint").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "models/Teddy.js"+str, true);
+  xhttp.send();
+}
+function showCustomer(str) {
+  var xhttp;
+  if (str == "") {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  }
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("txtHint").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "vu/Teddy.js"+str, true);
+  xhttp.send();
+}
--- front/app.js
+++ front/app.js
@@ -1,32 +1,110 @@
-//  function Produit (image,colors,price,description,name){
-//     this.image
-//     this.colors;
-//     this.price;
-//     this.description;
-//     this.name
-// }
-// const produit1 = new Produit("http://localhost:3000/images/teddy_1.jpg","Tan","Chocolate","Black","White", "Norbert",2900)
-// const produit2 = new Produit("http://localhost:3000/images/teddy_2.jpg")
-// const produit3 = new Produit("http://localhost:3000/images/teddy_3.jpg")
-// const produit4 = new Produit("http://localhost:3000/images/teddy_4.jpg")
-// const produit5 = new Produit("http://localhost:3000/images/teddy_5.jpg")
-// var Produit=[];
-// Produit.push(produit1);
-// console.log(Produit)
-// function Product(pic,name,price){
-//     this.pic = pic;
-//     this.name = name;
-//     this.price =price;
-//   }
-//   var nx310 = new Product("img/nx310.png",'nx 310',70);
-// var p5 = new Product('img/p5.png','p5',58);
-// var nsk4100 = new Product('img/nsk4100.jpg','nsk4100',60);
-// var vsk3000 = new Product('img/vsk3000elite.jpg','vsk3000',44.99)
 
-// let products = [];
-// var nx310 = new Product("img/nx310.png",'nx 310',70);
-// var p5 = new Product('img/p5.png','p5',58);
-// var nsk4100 = new Product('img/nsk4100.jpg','nsk4100',60);
-// var vsk3000 = new Product('img/vsk3000elite.jpg','vsk3000',44.99)
-// products.push(nx310,p5,nsk4100,vsk3000);
-// console.log(products)
+var request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+var response = JSON.parse(this.responseText);
+// console.log(response);
+}
+
+
+
+
+var ul =document.createElement('ul');
+demo.appendChild(ul)
+
+for(i=4;i<response.length;i++){
+    var li = document.createElement('li')
+    li.textContent = response[0].price + " ______  "+ response[0].name +"_______"+response[0].description+ "_______"+response[0].colors 
+    console.log(response[i].name)
+    ul.appendChild(li)
+} 
+var ul =document.createElement('ul');
+test.appendChild(ul)
+for(i=4;i<response.length;i++){
+    var li = document.createElement('li')
+    li.textContent = response[1].price + " ______  "+ response[1].name +"_______"+response[1].description+ "_______"+response[1].colors 
+    console.log(response[i].name)
+    ul.appendChild(li)
+} 
+var ul =document.createElement('ul');
+hsp.appendChild(ul)
+
+for(i=4;i<response.length;i++){
+    var li = document.createElement('li')
+    li.textContent = response[1].price + " ______  "+ response[2].name +"_______"+response[2].description+ "_______"+response[2].colors 
+    console.log(response[i].name)
+    ul.appendChild(li)
+} 
+var ul =document.createElement('ul');
+toto.appendChild(ul)
+for(i=4;i<response.length;i++){
+    var li = document.createElement('li')
+    li.textContent = response[3].price + " ______  "+ response[3].name +"_______"+response[3].description+ "_______"+response[3].colors 
+    console.log(response[i].name)
+    ul.appendChild(li)
+} 
+var ul =document.createElement('ul');
+tata.appendChild(ul)
+
+for(i=4;i<response.length;i++){
+    var li = document.createElement('li')
+    li.textContent = response[4].price + " ______  "+ response[4].name +"_______"+response[4].description+ "_______"+response[4].colors 
+    console.log(response[i].name)
+    ul.appendChild(li)
+} 
+
+      
+// var ul =document.createElement('ul');
+// demo.appendChild(ul)
+
+
+// // }
+// var ul =document.createElement('ul');
+// demo.appendChild(ul)
+// // for(i=0;i<response.length;i++){
+//     // var li = document.createElement('li');
+//  li.innerHTML = response[0].description
+//  console.log(response[0].description)
+//     //  ul.appendChild(li)
+
+// var ul =document.createElement('ul');
+// var ul =document.createElement('ul');
+// demo.appendChild(ul)
+// // for(i=0;i<response.length;i++){
+//     var li = document.createElement('li')
+//     li.innerHTML = response[0].name
+//     // console.log(response[0].name)
+
+//      ul.appendChild(li)
+//      var ul =document.createElement('ul');
+//      demo.appendChild(ul)
+//     //  for(i=0;i<response.length;i++){
+//          var li = document.createElement('li')
+//          li.innerHTML = response[0].price
+//           ul.appendChild(li)
+
+}
+ 
+
+request.open("GET", "http://localhost:3000/api/teddies");
+request.send();
+// for(i=0;i<response.length;i++){
+//     demo.appendChild(ul)
+//     var ul =document.createElement('ul');
+
+//         var li = document.createElement('li')
+//         li.innerHTML = response[i]
+//          li.innerHTML = response[i].name
+//         // li.innerHTML = response[2]
+//         // li.innerHTML = response[3]
+//         // li.innerHTML = response[4]
+//         console.log(response[i].name)
+//         // console.log(response[1])
+//         // console.log(response[2])
+//         // console.log(response[3])
+//         // console.log(response[4])
+
+
+
+//         ul.appendChild('li')
+// }
