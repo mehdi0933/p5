9e8f8866b0264e0b9e13bf8b61a14ede9ea6e650
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2020-10-27 22:11:09 +0100

    Add files via upload

--- front/app.js
+++ front/app.js
@@ -6,105 +6,72 @@ var response = JSON.parse(this.responseText);
 // console.log(response);
 }
 
+var div =document.createElement('div');
+demo.appendChild(div)
 
-
-
-var ul =document.createElement('ul');
-demo.appendChild(ul)
-
-for(i=4;i<response.length;i++){
-    var li = document.createElement('li')
-    li.textContent = response[0].price + " ______  "+ response[0].name +"_______"+response[0].description+ "_______"+response[0].colors 
-    console.log(response[i].name)
-    ul.appendChild(li)
-} 
-var ul =document.createElement('ul');
-test.appendChild(ul)
-for(i=4;i<response.length;i++){
-    var li = document.createElement('li')
-    li.textContent = response[1].price + " ______  "+ response[1].name +"_______"+response[1].description+ "_______"+response[1].colors 
-    console.log(response[i].name)
-    ul.appendChild(li)
-} 
-var ul =document.createElement('ul');
-hsp.appendChild(ul)
-
-for(i=4;i<response.length;i++){
-    var li = document.createElement('li')
-    li.textContent = response[1].price + " ______  "+ response[2].name +"_______"+response[2].description+ "_______"+response[2].colors 
-    console.log(response[i].name)
-    ul.appendChild(li)
-} 
-var ul =document.createElement('ul');
-toto.appendChild(ul)
-for(i=4;i<response.length;i++){
-    var li = document.createElement('li')
-    li.textContent = response[3].price + " ______  "+ response[3].name +"_______"+response[3].description+ "_______"+response[3].colors 
-    console.log(response[i].name)
-    ul.appendChild(li)
-} 
-var ul =document.createElement('ul');
-tata.appendChild(ul)
-
-for(i=4;i<response.length;i++){
-    var li = document.createElement('li')
-    li.textContent = response[4].price + " ______  "+ response[4].name +"_______"+response[4].description+ "_______"+response[4].colors 
-    console.log(response[i].name)
-    ul.appendChild(li)
-} 
-
-      
-// var ul =document.createElement('ul');
-// demo.appendChild(ul)
-
-
-// // }
-// var ul =document.createElement('ul');
-// demo.appendChild(ul)
-// // for(i=0;i<response.length;i++){
-//     // var li = document.createElement('li');
-//  li.innerHTML = response[0].description
-//  console.log(response[0].description)
-//     //  ul.appendChild(li)
-
-// var ul =document.createElement('ul');
-// var ul =document.createElement('ul');
-// demo.appendChild(ul)
-// // for(i=0;i<response.length;i++){
-//     var li = document.createElement('li')
-//     li.innerHTML = response[0].name
-//     // console.log(response[0].name)
-
-//      ul.appendChild(li)
-//      var ul =document.createElement('ul');
-//      demo.appendChild(ul)
-//     //  for(i=0;i<response.length;i++){
-//          var li = document.createElement('li')
-//          li.innerHTML = response[0].price
-//           ul.appendChild(li)
-
+for(i=0;i<response.length;i++){
+    var nounous1= document.getElementsByClassName("nounous1")
+    nounous1[0].innerHTML =response[0].price + " ______  "+ response[0].name +"_______"+response[0].description+ "_______"+response[0].colors
 }
- 
+
+for(i=0;i<response.length;i++){
+    var div =document.createElement('div');
+test.appendChild(div)
+    var nounous2= document.getElementsByClassName("nounous1")
+    nounous1[1].innerHTML =response[1].price + " ______  "+ response[1].name +"_______"+response[1].description+ "_______"+response[1].colors
+    console.log(nounous1[1].innerHTML)
+} 
+var div =document.createElement('div');
+hsp.appendChild(div)
+
+for(i=0;i<response.length;i++){
+    var nounous1= document.getElementsByClassName("nounous1")
+    nounous1[2].innerHTML =response[2].price + " ______  "+ response[2].name +"_______"+response[2].description+ "_______"+response[2].colors
+} 
+var div =document.createElement('div');
+toto.appendChild(div)
+for(i=4;i<response.length;i++){
+    var nounous1= document.getElementsByClassName("nounous1")
+    nounous1[3].innerHTML =response[3].price + " ______  "+ response[3].name +"_______"+response[3].description+ "_______"+response[3].colors
+} 
+var div =document.createElement('div');
+tata.appendChild(div)
+
+for(i=4;i<response.length;i++){
+    var nounous1= document.getElementsByClassName("nounous1")
+    nounous1[4].innerHTML =response[4].price + " ______  "+ response[4].name +"_______"+response[4].description+ "_______"+response[4].colors
+} 
+
+
+ }
+
+ function peluche(evt, nounou) {
+    var i, article, boutton;
+    article = document.getElementsByClassName("article");
+    for (i = 0; i < article.length; i++) {
+      article[i].style.display = "none";
+    }
+    boutton = document.getElementsByClassName("boutton");
+    for (i = 0; i < boutton.length; i++) {
+      boutton[i].className = boutton[i].className.replace(" active", "");
+    }
+    document.getElementById(nounou).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+// function peluche(evt, nounou) {
+//     var i, article, boutton;
+//     article = document.getElementsByClassName("article");
+//     for (i = 0; i < article.length; i++) {
+//       article[i].style.display = "none";
+//     }
+//     boutton = document.getElementsByClassName("boutton");
+//     for (i = 0; i < boutton.length; i++) {
+//       boutton[i].className = boutton[i].className.replace(" active", "");
+//     }
+//     document.getElementById(openCity).style.display = "block";
+//     evt.currentTarget.className += " active";
+// }
 
 request.open("GET", "http://localhost:3000/api/teddies");
 request.send();
-// for(i=0;i<response.length;i++){
-//     demo.appendChild(ul)
-//     var ul =document.createElement('ul');
-
-//         var li = document.createElement('li')
-//         li.innerHTML = response[i]
-//          li.innerHTML = response[i].name
-//         // li.innerHTML = response[2]
-//         // li.innerHTML = response[3]
-//         // li.innerHTML = response[4]
-//         console.log(response[i].name)
-//         // console.log(response[1])
-//         // console.log(response[2])
-//         // console.log(response[3])
-//         // console.log(response[4])
-
-
-
-//         ul.appendChild('li')
-// }
