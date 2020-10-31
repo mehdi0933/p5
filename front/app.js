1ae89932b431580bdc9dd25c9bfc61a7763d2509
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2020-10-31 18:28:48 +0100

    Add files via upload

--- front/app.js
+++ front/app.js
@@ -6,42 +6,20 @@ var response = JSON.parse(this.responseText);
 // console.log(response);
 }
 
-var div =document.createElement('div');
-demo.appendChild(div)
+
 
 for(i=0;i<response.length;i++){
     var nounous1= document.getElementsByClassName("nounous1")
     nounous1[0].innerHTML =response[0].price + " ______  "+ response[0].name +"_______"+response[0].description+ "_______"+response[0].colors
-}
-
-for(i=0;i<response.length;i++){
-    var div =document.createElement('div');
-test.appendChild(div)
-    var nounous2= document.getElementsByClassName("nounous1")
+    var nounous1= document.getElementsByClassName("nounous1")
     nounous1[1].innerHTML =response[1].price + " ______  "+ response[1].name +"_______"+response[1].description+ "_______"+response[1].colors
-    console.log(nounous1[1].innerHTML)
-} 
-var div =document.createElement('div');
-hsp.appendChild(div)
-
-for(i=0;i<response.length;i++){
     var nounous1= document.getElementsByClassName("nounous1")
     nounous1[2].innerHTML =response[2].price + " ______  "+ response[2].name +"_______"+response[2].description+ "_______"+response[2].colors
-} 
-var div =document.createElement('div');
-toto.appendChild(div)
-for(i=4;i<response.length;i++){
     var nounous1= document.getElementsByClassName("nounous1")
     nounous1[3].innerHTML =response[3].price + " ______  "+ response[3].name +"_______"+response[3].description+ "_______"+response[3].colors
-} 
-var div =document.createElement('div');
-tata.appendChild(div)
-
-for(i=4;i<response.length;i++){
     var nounous1= document.getElementsByClassName("nounous1")
     nounous1[4].innerHTML =response[4].price + " ______  "+ response[4].name +"_______"+response[4].description+ "_______"+response[4].colors
-} 
-
+}
 
  }
 
