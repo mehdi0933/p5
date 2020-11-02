2fd01ef65153ce9e823e541a70fe771e031ed3c2
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2020-11-02 18:06:08 +0100

    Add files via upload

--- front/app.js
+++ front/app.js
@@ -1,4 +1,4 @@
-
+//ajax
 var request = new XMLHttpRequest();
 request.onreadystatechange = function () {
 if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
@@ -6,46 +6,16 @@ var response = JSON.parse(this.responseText);
 // console.log(response);
 }
 
-var div =document.createElement('div');
-demo.appendChild(div)
 
+//les object sur le prix coleur nom description
 for(i=0;i<response.length;i++){
     var nounous1= document.getElementsByClassName("nounous1")
-    nounous1[0].innerHTML =response[0].price + " ______  "+ response[0].name +"_______"+response[0].description+ "_______"+response[0].colors
+    nounous1[i].innerHTML =response[i].price + " ______  "+ response[i].name +"_______"+response[i].description+ "_______"+response[i].colors
 }
-
-for(i=0;i<response.length;i++){
-    var div =document.createElement('div');
-test.appendChild(div)
-    var nounous2= document.getElementsByClassName("nounous1")
-    nounous1[1].innerHTML =response[1].price + " ______  "+ response[1].name +"_______"+response[1].description+ "_______"+response[1].colors
-    console.log(nounous1[1].innerHTML)
-} 
-var div =document.createElement('div');
-hsp.appendChild(div)
-
-for(i=0;i<response.length;i++){
-    var nounous1= document.getElementsByClassName("nounous1")
-    nounous1[2].innerHTML =response[2].price + " ______  "+ response[2].name +"_______"+response[2].description+ "_______"+response[2].colors
-} 
-var div =document.createElement('div');
-toto.appendChild(div)
-for(i=4;i<response.length;i++){
-    var nounous1= document.getElementsByClassName("nounous1")
-    nounous1[3].innerHTML =response[3].price + " ______  "+ response[3].name +"_______"+response[3].description+ "_______"+response[3].colors
-} 
-var div =document.createElement('div');
-tata.appendChild(div)
-
-for(i=4;i<response.length;i++){
-    var nounous1= document.getElementsByClassName("nounous1")
-    nounous1[4].innerHTML =response[4].price + " ______  "+ response[4].name +"_______"+response[4].description+ "_______"+response[4].colors
-} 
-
 
  }
 
-
+// fenetre modal
     window.onclick = function(event) {
       if (event.target == modal) {
         modal.style.display = "none";
