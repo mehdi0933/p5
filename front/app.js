42136db73c9a5d74b8011882c8ed0ff49cadb918
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2020-11-04 00:10:30 +0100

    Add files via upload

--- front/app.js
+++ front/app.js
@@ -16,11 +16,35 @@ for(i=0;i<response.length;i++){
  }
 
 // fenetre modal
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
+    // window.onclick = function(event) {
+    //   if (event.target == modal) {
+    //     modal.style.display = "none";
+    //   }
+    // }
+//
+
+var commande = document.getElementsByClassName('commande')
+for(var i=0;i<commande.length;i++){
+  commande[i].addEventListener('click',function() {
+    //  console.log("commande")
+    NombreCommande()
+  })
+}
+function NombreCommande(){
+  var produireNombre = localStorage.getItem('NombreCommande')
+ 
+  produireNombre=parseInt(produireNombre)
+  if(produireNombre){
+    localStorage.setItem('NombreCommande',produireNombre+1)
+    document.getElementsByClassName('compte').innerHTML= produireNombre+ 1
+  }
+
+else{localStorage.setItem('NombreCommande',1)
+document.getElementsByClassName('compte').innerHTML= 1
+} 
+}
 
 request.open("GET", "http://localhost:3000/api/teddies");
 request.send();
+
+  
