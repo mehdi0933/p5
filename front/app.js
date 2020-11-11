8e6ce0092b74319b97ce53dd5b3b52e3f1e2d9eb
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2020-11-11 22:33:30 +0100

    Add files via upload

--- front/app.js
+++ front/app.js
@@ -1,19 +1,51 @@
 //ajax
-var request = new XMLHttpRequest();
-request.onreadystatechange = function () {
-if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-var response = JSON.parse(this.responseText);
-// console.log(response);
+fetch('http://localhost:3000/api/teddies' )
+  .then(response => response.json())
+  .then ( function (data){
+    let paragraphe ;
+    for(i=0;i<data.length;i++){
+      // var nounous1= document.getElementsByClassName("nounous1")
+      // nounous1[i].innerHTML ="price : "+response[i].price +"$"+ "<br>  " +"name : "+ response[i].name +"<br>" +"description : "+ response[i].description+ "<br>"+"colors : "+ response[i].colors
+    console.log(data[i].name);
+    paragraphe=document.createElement ('p');
+    paragraphe.textContent='name :'+data[i].name
+    document.getElementById("contenu").appendChild(paragraphe);
+    console.log(data[i]._id);
+paragraphe=document.createElement ('p');
+paragraphe.textContent='ref produit :'+data[i]._id
+document.getElementById("contenu").appendChild(paragraphe);
+
+
+console.log(data[i].price);
+paragraphe=document.createElement ('p');
+paragraphe.textContent='price :'+ data[i].price +'$'
+document.getElementById("contenu").appendChild(paragraphe);
+
+console.log(data[i].description);
+paragraphe=document.createElement ('p');
+paragraphe.textContent='description :'+ data[i].description
+document.getElementById("contenu").appendChild(paragraphe);
+
+console.log(data[i].imageUrl);
+paragraphe=document.createElement ('p')
+paragraphe.innerHTML= `<img src =`+ data[i].imageUrl+`>`
+document.getElementById("contenu").appendChild(paragraphe);
+
 }
+    
+
+  })
+  
 
 
-//les object sur le prix coleur nom description
-for(i=0;i<response.length;i++){
-    var nounous1= document.getElementsByClassName("nounous1")
-    nounous1[i].innerHTML ="price : "+response[i].price +"$"+ "<br>  " +"name : "+ response[i].name +"<br>" +"description : "+ response[i].description+ "<br>"+"colors : "+ response[i].colors
-}
 
- }
+
+// }
+// }
+
+// imageElement.src =
+
+ 
 
 // fenetre modal
     // window.onclick = function(event) {
@@ -23,28 +55,30 @@ for(i=0;i<response.length;i++){
     // }
 //
 
-var commande = document.getElementsByClassName('commande')
-for(var i=0;i<commande.length;i++){
-  commande[i].addEventListener('click',function() {
-    //  console.log("commande")
-    NombreCommande()
-  })
-}
-function NombreCommande(){
-  var produireNombre = localStorage.getItem('NombreCommande')
- 
-  produireNombre=parseInt(produireNombre)
-  if(produireNombre){
-    localStorage.setItem('NombreCommande',produireNombre+1)
-    document.getElementsByClassName('compte').innerHTML= produireNombre+ 1
-  }
+// var commande = document.getElementsByClassName('commande');
+// for(var i=0;i<commande.length;i++){
+//   commande[i].addEventListener('click',function() {
+//       // console.log("commande")
+//     NombreCommande();
+//   })
+// }
+// function NombreCommande(){
+//   var produireNombre = localStorage.getItem('NombreCommande');
+//   console.log(produireNombre);
+//   produireNombre=parseInt(produireNombre);
+//   if(produireNombre){
+//     localStorage.setItem('NombreCommande',produireNombre + 1);
+//     document.getElementsByClassName('compte').innerHTML= produireNombre + 1;
+//   }
 
-else{localStorage.setItem('NombreCommande',1)
-document.getElementsByClassName('compte').innerHTML= 1
-} 
-}
-
-request.open("GET", "http://localhost:3000/api/teddies");
-request.send();
+// else{localStorage.setItem('NombreCommande',1);
+// document.getElementsByClassName('compte').innerHTML= 1;
+// } 
+// }
+// panier vide 
+//selectionne les produits section storige
+//au clique on l'envoie au panier
+// request.open("GET", "http://localhost:3000/api/teddies");
+// request.send();
 
   
